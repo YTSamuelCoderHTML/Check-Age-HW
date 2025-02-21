35ccdbb17d0e6702efc529561313f2f9ab7cdeb8
YTSamuelCoderHTML <muhammadsaimnaeem481@gmail.com>
2025-02-21 17:07:04 +0500

    Add files via upload

---
+++ age.py
@@ -0,0 +1,10 @@
+age = int(input("Enter an age from 10 to 20:"))
+
+if age < 10:
+    print("The number is less than 10!")
+
+if age > 20:
+    print("The number is more than 20!")
+
+if (age >= 10 and age <= 20):
+    print("Okay!")
